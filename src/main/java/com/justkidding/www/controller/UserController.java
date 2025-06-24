35c7238315bf0ffacb694c7ef093ba5673e0caaa
benisamuel <benisamuel566@gmail.com>
2025-06-24 21:56:20 +0200

    added user controller methods

--- src/main/java/com/justkidding/www/controller/UserController.java
+++ src/main/java/com/justkidding/www/controller/UserController.java
@@ -1,4 +1,37 @@
 package com.justkidding.www.controller;
 
+import com.justkidding.www.model.User;
+import com.justkidding.www.service.UserService;
+import com.justkidding.www.utils.ApiResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/justkidding/user")
+@AllArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse<List<User>>> getAllUsers () {
+        return ResponseEntity.ok(new ApiResponse<>(true, "Successfully retrieved all users!!! 🎉🎉🎉", this.userService.getAllUsers()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<User>> getUserById (@PathVariable Long id) {
+        User user = this.userService.getUserById(id);
+        if (user != null) {
+            return ResponseEntity.ok(new ApiResponse<>(true, "Successfully obtained user by id!!! 🎉🎉🎉", user));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, "Failed obtain user not found!!! 😔💔💔", null));
+    }
+
+
 }

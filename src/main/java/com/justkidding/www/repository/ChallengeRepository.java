242b98658b61a399f22bd558d7cea51585fc0e5c
benisamuel <benisamuel566@gmail.com>
2025-06-26 13:37:25 +0200

    added repository

--- src/main/java/com/justkidding/www/repository/ChallengeRepository.java
+++ src/main/java/com/justkidding/www/repository/ChallengeRepository.java
@@ -1,7 +1,11 @@
 package com.justkidding.www.repository;
 
 import com.justkidding.www.model.Challenge;
+import com.justkidding.www.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+    List<Challenge> getChallengeByAuthor(User user);
 }
--- src/main/java/com/justkidding/www/repository/PostRepository.java
+++ src/main/java/com/justkidding/www/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package com.justkidding.www.repository;
 
 import com.justkidding.www.model.Post;
+import com.justkidding.www.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Optional<Post> getPostById (Long id);
+    List<Post> getPostsByAuthor (User user);
 }

867d69cae3609685a240bb0eeb22cce545ceb6b5
benisamuel <benisamuel566@gmail.com>
2025-06-30 19:37:17 +0200

    added customeruserdetailsservice

--- src/main/java/com/justkidding/www/security/CustomUserDetailsService.java
+++ src/main/java/com/justkidding/www/security/CustomUserDetailsService.java
@@ -1,14 +1,25 @@
 package com.justkidding.www.security;
 
+import com.justkidding.www.exception.NotFoundException;
+import com.justkidding.www.model.User;
+import com.justkidding.www.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = this.userService.getUserByEmail(username);
+        if (user != null) {
+            return new CustomUserDetails(user);
+        }
+        throw new NotFoundException("Sorry user not found!!!");
     }
 }

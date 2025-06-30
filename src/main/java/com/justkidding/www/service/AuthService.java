89db660b55b32574fb3fb4f043719dc3efc0b151
benisamuel <benisamuel566@gmail.com>
2025-06-30 19:37:40 +0200

    added file and auth service

--- src/main/java/com/justkidding/www/service/AuthService.java
+++ src/main/java/com/justkidding/www/service/AuthService.java
@@ -1,9 +1,16 @@
 package com.justkidding.www.service;
 
+import com.justkidding.www.dto.LoginDto;
 import com.justkidding.www.dto.RegisterDto;
 import com.justkidding.www.model.User;
+import com.justkidding.www.utils.ApiResponse;
+import com.justkidding.www.utils.JwtUtil;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +18,21 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     public User registerUser (RegisterDto registerDto) {
         return this.userService.createUser(registerDto);
     }
 
-    public String loginUser () {
-        return "token";
+    public ResponseEntity<ApiResponse<String>> loginUser (LoginDto loginDto) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken (loginDto.getEmail(), loginDto.getPassword())
+            );
+            User user = this.userService.getUserByEmail(loginDto.getEmail());
+            return ResponseEntity.ok(new ApiResponse<>(true, "Successfully logged in a user!!!", this.jwtUtil.generateToken(user.getEmail())));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(false,"Invalid credentials provided check your request!!!", null));
+        }
     }
 }
--- src/main/java/com/justkidding/www/service/FileService.java
+++ src/main/java/com/justkidding/www/service/FileService.java
@@ -4,7 +4,6 @@ import com.justkidding.www.model.Post;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 

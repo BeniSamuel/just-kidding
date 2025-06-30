327724a3d7107c45ec2b46dcc26fa5d19971d017
benisamuel <benisamuel566@gmail.com>
2025-06-30 19:38:02 +0200

    added jwtutil

--- src/main/java/com/justkidding/www/utils/JwtUtil.java
+++ src/main/java/com/justkidding/www/utils/JwtUtil.java
@@ -1,4 +1,31 @@
 package com.justkidding.www.utils;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+
+    @Value("${jwt.secret.key}")
+    private String JWT_SECRET;
+
+    public String generateToken (String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS512, JWT_SECRET)
+                .compact();
+    }
+
+    public String extractToken (String token) {
+        return Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
 }

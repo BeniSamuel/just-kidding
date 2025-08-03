1794f286d998cdb519e3bdb7e984a243ab47afda
benisamuel <benisamuel566@gmail.com>
2025-08-03 22:19:33 +0200

    added comment and like service

--- src/main/java/com/justkidding/www/service/CommentService.java
+++ src/main/java/com/justkidding/www/service/CommentService.java
@@ -1,4 +1,58 @@
 package com.justkidding.www.service;
 
+import com.justkidding.www.dto.CommentDto;
+import com.justkidding.www.model.Comment;
+import com.justkidding.www.model.Post;
+import com.justkidding.www.model.User;
+import com.justkidding.www.repository.CommentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class CommentService {
+    private final CommentRepository commentRepository;
+    private final UserService userService;
+    private final PostService postService;
+
+    public List<Comment> getAllComments () {
+        return this.commentRepository.findAll();
+    }
+
+    public Comment getCommentById (Long id) {
+        return this.commentRepository.getCommentById(id).orElse(null);
+    }
+
+    public List<Comment> getCommentByUser (Long user_id) {
+        User user = this.userService.getUserById(user_id);
+        if (user != null) {
+            return this.commentRepository.getCommentsByAuthor(user);
+        }
+        return null;
+    }
+
+    public List<Comment> getCommentByPost (Long post_id) {
+        Post post = this.postService.getPostById(post_id);
+        if (post != null) {
+            return this.commentRepository.getCommentsByPost(post);
+        }
+        return null;
+    }
+
+    public Comment createComment (CommentDto commentDto) {
+        User author = this.userService.getUserById(commentDto.getUser_id());
+        if (author == null) {
+            return null;
+        }
+
+        Post post = this.postService.getPostById(commentDto.getPost_id());
+        if (post == null) {
+            return null;
+        }
+
+        Comment newComment = new Comment(commentDto.getStatement(), author, post);
+        return this.commentRepository.save(newComment);
+    }
 }
--- src/main/java/com/justkidding/www/service/LikeService.java
+++ src/main/java/com/justkidding/www/service/LikeService.java
@@ -1,4 +1,67 @@
 package com.justkidding.www.service;
 
+import com.justkidding.www.dto.LikeDto;
+import com.justkidding.www.model.Like;
+import com.justkidding.www.model.Post;
+import com.justkidding.www.model.User;
+import com.justkidding.www.repository.LikeRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class LikeService {
+    private final LikeRepository likeRepository;
+    private final PostService postService;
+    private final UserService userService;
+
+    public List<Like> getAllLikes () {
+        return this.likeRepository.findAll();
+    }
+
+    public Like getLikeById (Long id) {
+        return this.likeRepository.findById(id).orElse(null);
+    }
+
+    public List<Like> getAllLikesByPost (Long post_id) {
+        Post post = this.postService.getPostById(post_id);
+        if (post != null) {
+            return this.likeRepository.getLikesByPost(post);
+        }
+        return null;
+    }
+
+    public List<Like> getAllLikesByUser (Long user_id) {
+        User user = this.userService.getUserById(user_id);
+        if (user != null) {
+            return this.likeRepository.getLikesByAuthor(user);
+        }
+        return null;
+    }
+
+    public Like createLike (LikeDto likeDto) {
+        User user = this.userService.getUserById(likeDto.getUser_id());
+        if (user == null) {
+            return null;
+        }
+
+        Post post = this.postService.getPostById(likeDto.getPost_id());
+        if (post == null) {
+            return null;
+        }
+
+        Like newLike = new Like(user, post);
+        return this.likeRepository.save(newLike);
+    }
+
+    public Boolean deleteLike (Long id) {
+        Like like = this.getLikeById(id);
+        if (like != null) {
+            this.likeRepository.delete(like);
+            return true;
+        }
+        return false;
+    }
 }

6ee929f7099fe0eb96481d3e6ab6b71096e6f0ec
benisamuel <benisamuel566@gmail.com>
2025-08-03 20:44:47 +0200

    Modified securify config to have restriction on some routes

--- src/main/java/com/justkidding/www/configuration/SecurityConfig.java
+++ src/main/java/com/justkidding/www/configuration/SecurityConfig.java
@@ -1,25 +1,36 @@
 package com.justkidding.www.configuration;
 
+import com.justkidding.www.security.JwtAuthFilter;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/api/justkidding/auth/register").permitAll()
+                        .requestMatchers("/api/justkidding/auth/login").permitAll()
+                        .requestMatchers("/api/justkidding/challenge/create").hasRole("ADMIN")
+                        .anyRequest().authenticated()
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
---
+++ src/main/java/com/justkidding/www/controller/LikeController.java
@@ -0,0 +1,4 @@
+package com.justkidding.www.controller;
+
+public class LikeController {
+}

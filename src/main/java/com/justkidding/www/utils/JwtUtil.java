a1dcdb060b262de6f259809aa292fd260847231d
benisamuel <benisamuel566@gmail.com>
2025-08-03 22:20:05 +0200

    added jwtUtil

--- src/main/java/com/justkidding/www/utils/JwtUtil.java
+++ src/main/java/com/justkidding/www/utils/JwtUtil.java
@@ -1,8 +1,10 @@
 package com.justkidding.www.utils;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -27,5 +29,24 @@ public class JwtUtil {
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
+    }
+
+    public boolean validateToken (String token, UserDetails userDetails) {
+        try {
+            String email = this.extractToken(token);
+            return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    public boolean isTokenExpired (String token) {
+        return Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
+
     }
 }

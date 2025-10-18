0ead06831a9490829b03c90c022d6867dc1ddf1c
benisamuel <benisamuel566@gmail.com>
2025-10-18 21:19:45 +0200

    added @AllArgsConstructor

--- src/main/java/com/justkidding/www/dto/RegisterDto.java
+++ src/main/java/com/justkidding/www/dto/RegisterDto.java
@@ -1,9 +1,11 @@
 package com.justkidding.www.dto;
 
 import com.justkidding.www.enums.Role;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class RegisterDto {
     private String name;
     private String email;
